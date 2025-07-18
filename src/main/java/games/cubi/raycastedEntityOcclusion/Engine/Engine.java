963fbbe9be457caf76def533f4368c30a65c69d6
Cubicake <74905@stpeters.sa.edu.au>
2025-07-18 16:42:29 +0930

    Create new Engine

---
+++ src/main/java/games/cubi/raycastedEntityOcclusion/Engine/Engine.java
@@ -0,0 +1,25 @@
+package games.cubi.raycastedEntityOcclusion.Engine;
+
+import games.cubi.raycastedEntityOcclusion.ConfigManager;
+import games.cubi.raycastedEntityOcclusion.RaycastedEntityOcclusion;
+import games.cubi.raycastedEntityOcclusion.Utils.*;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Engine {
+    //TODO: Maybe store the data somewhere else? Could rework chunk storage to be general data storage
+    ConcurrentHashMap<UUID, PlayerData> playerDataMap = new ConcurrentHashMap<>();
+
+    private final RaycastedEntityOcclusion plugin;
+    private final ConfigManager config;
+
+    public Engine(RaycastedEntityOcclusion plugin, ConfigManager cfg) {
+        this.plugin = plugin;
+        this.config = cfg;
+    }
+
+    public void registerPlayer(UUID playerUUID) {
+        playerDataMap.putIfAbsent(playerUUID, new PlayerData(playerUUID));
+    }
+}
