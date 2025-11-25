26a4d4c10c8ebb3368031fc694a89046a9f2842d
Cubicake <74905@stpeters.sa.edu.au>
2025-11-25 22:03:19 +1030

    Modify TileEntitySnapshotManager interface to add methods needed for engine

--- src/main/java/games/cubi/raycastedAntiESP/snapshot/TileEntitySnapshotManager.java
+++ src/main/java/games/cubi/raycastedAntiESP/snapshot/TileEntitySnapshotManager.java
@@ -3,12 +3,11 @@ package games.cubi.raycastedAntiESP.snapshot;
 import games.cubi.raycastedAntiESP.utils.BlockLocation;
 import games.cubi.raycastedAntiESP.utils.Locatable;
 
-import org.bukkit.World;
-
 import java.util.Set;
 import java.util.UUID;
 
 public interface TileEntitySnapshotManager {
     public Set<UUID> getPlayersWhoCanSee(Locatable tileEntity);
-    public Set<BlockLocation> getTileEntitiesInChunk(World world, int x, int z);
+    public int getTicksSincePlayerSawTileEntity(UUID player, Locatable tileEntity);
+    public Set<BlockLocation> getTileEntitiesInChunk(UUID world, int x, int z);
 }
