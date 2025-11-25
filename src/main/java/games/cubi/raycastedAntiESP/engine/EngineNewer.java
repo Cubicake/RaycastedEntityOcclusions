db13824a0f0fa3e46032e7ad341025957ef9c1a5
Cubicake <74905@stpeters.sa.edu.au>
2025-11-25 22:01:38 +1030

    Add process distribution over multiple threads, begin impl of actual checks

--- src/main/java/games/cubi/raycastedAntiESP/engine/EngineNewer.java
+++ src/main/java/games/cubi/raycastedAntiESP/engine/EngineNewer.java
@@ -4,18 +4,18 @@ import games.cubi.raycastedAntiESP.Logger;
 import games.cubi.raycastedAntiESP.config.*;
 import games.cubi.raycastedAntiESP.RaycastedAntiESP;
 import games.cubi.raycastedAntiESP.data.DataHolder;
+import games.cubi.raycastedAntiESP.snapshot.SnapshotManager;
+import games.cubi.raycastedAntiESP.utils.Locatable;
 import games.cubi.raycastedAntiESP.utils.PlayerData;
 import games.cubi.raycastedAntiESP.utils.ThreadSafeLoc;
 import io.papermc.paper.threadedregions.scheduler.AsyncScheduler;
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.scheduler.BukkitScheduler;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class EngineNewer {
@@ -37,15 +37,67 @@ public class EngineNewer {
         flushLogCache(null);
     }
 
-    public void tick() {
-        for (PlayerData playerData : DataHolder.players().getAllPlayerData()) {
-            if (playerData.hasBypassPermission()) continue;
-            ThreadSafeLoc playerLocation = DataHolder.entityLocation().getEntityLocation(playerData.getPlayerUUID());
-            if (playerLocation == null) Logger.errorAndReturn(new RuntimeException("wtf"));
+    //run async
+    private void distributeTick() {
+        Collection<PlayerData> allPlayers = DataHolder.players().getAllPlayerData();
+        int threads = 1; //TODO Don't hardcode
+        if (threads < 1) threads = 1;
+
+        List<List<PlayerData>> batches = new ArrayList<>(threads);
+        for (int i = 0; i < threads; i++) {
+            batches.add(new ArrayList<>());
+        }
+
+        int index = 0;
+        for (PlayerData playerData : allPlayers) {
+            batches.get(index++ % threads).add(playerData);
+        }
+
+        EntityConfig entityConfig = config.getEntityConfig();
+        PlayerConfig playerConfig = config.getPlayerConfig();
+        TileEntityConfig tileEntityConfig = config.getTileEntityConfig();
+        int maxRadius = Bukkit.getViewDistance() * 16;
+        int tileEntityRadius = (Math.max(tileEntityConfig.getRaycastRadius(), maxRadius)+15)/16; //Fine to precompute this stuff cos a single division per tick is negligible
+
+        for (List<PlayerData> batch : batches) {
+            plugin.getServer().getAsyncScheduler().runNow(plugin, task -> {
+                processTickForPlayers(batch, entityConfig, playerConfig, tileEntityConfig, tileEntityRadius);
+                task.cancel();
+            });
         }
     }
 
-    private void /*TODO: return the subscriber list*/ createSubscriberList() {
+    private void processTickForPlayers(List<PlayerData> playerDataList, EntityConfig entityConfig, PlayerConfig playerConfig, TileEntityConfig tileEntityConfig, int tileEntityRadius) {
+        for (PlayerData playerData : playerDataList) {
+            if (playerData.hasBypassPermission()) continue;
+
+            Locatable playerLocation = SnapshotManager.get().getEntitySnapshotManager().getLocation(playerData.getPlayerUUID());
+            if (playerLocation == null) Logger.errorAndReturn(new RuntimeException("Player "+playerData.getPlayerUUID()+" does not have a location"));
+
+            if (entityConfig.isEnabled()) checkEntities(playerData, playerLocation, entityConfig);
+            if (playerConfig.isEnabled()) checkPlayers(playerData, playerLocation, playerConfig);
+            if (tileEntityConfig.isEnabled()) {checkTileEntities(playerData, playerLocation, tileEntityConfig, tileEntityRadius);}
+        }
+    }
+
+    private void checkEntities(PlayerData player, Locatable playerLocation, EntityConfig entityConfig) {
+        for (UUID entityUUID : player.getEntitiesNeedingRecheck(entityConfig.getVisibleRecheckInterval())) {
+
+        }
+    }
+
+    private void checkPlayers(PlayerData player, Locatable playerLocation, PlayerConfig playerConfig) {
+        for (UUID playerUUID : player.getPlayersNeedingRecheck(playerConfig.getVisibleRecheckInterval())) {
+
+        }
+    }
+
+    private void checkTileEntities(PlayerData player, Locatable playerLocation, TileEntityConfig tileEntityConfig, int radius) {
+        //todo: How to get tile entities around player? Just chunkscan?
+    }
+
+    /*
+    private void /\*TODO: return the subscriber list*\/ createSubscriberList() {
         for (PlayerData playerData : DataHolder.players().getAllPlayerData()) {
             if (playerData.hasBypassPermission()) continue;
             ThreadSafeLoc playerLocation = DataHolder.entityLocation().getEntityLocation(playerData.getPlayerUUID());
@@ -56,7 +108,7 @@ public class EngineNewer {
             boolean checkBlocks = config.getTileEntityConfig().isEnabled();
             boolean checkPlayers = config.getPlayerConfig().isEnabled();
         }
-    }
+    }*/
 
 
 
