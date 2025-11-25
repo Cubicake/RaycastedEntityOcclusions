a495d3946d553eccf85a49eda9c0cbd48a0e6320
Cubicake <74905@stpeters.sa.edu.au>
2025-11-25 22:04:54 +1030

    Create util class for handling visibility change

--- src/main/java/games/cubi/raycastedAntiESP/visibilitychangehandlers/VisibilityChangeHandlers.java
+++ src/main/java/games/cubi/raycastedAntiESP/visibilitychangehandlers/VisibilityChangeHandlers.java
@@ -1,14 +1,34 @@
 package games.cubi.raycastedAntiESP.visibilitychangehandlers;
 
+import org.jspecify.annotations.Nullable;
+
 public class VisibilityChangeHandlers {
-    private static VisibilityChangeHandlers instance;
+    private static EntityVisibilityChanger entityVisibilityChanger;
+    private static PlayerVisibilityChanger playerVisibilityChanger;
+    private static TileEntityVisibilityChanger tileEntityVisibilityChanger;
 
-    private VisibilityChangeHandlers() {}
+    public static void initialise(EntityVisibilityChanger entityVisibilityChanger1, PlayerVisibilityChanger playerVisibilityChanger1, TileEntityVisibilityChanger tileEntityVisibilityChanger1) {
+        changeHandlers(entityVisibilityChanger1, playerVisibilityChanger1, tileEntityVisibilityChanger1);
+    }
 
-    public static VisibilityChangeHandlers get() {
-        if (instance == null) {
-            instance = new VisibilityChangeHandlers();
-        }
-        return instance;
+    @Nullable
+    public static EntityVisibilityChanger getEntity() {
+        return entityVisibilityChanger;
+    }
+
+    @Nullable
+    public static PlayerVisibilityChanger getPlayer() {
+        return playerVisibilityChanger;
+    }
+
+    @Nullable
+    public static TileEntityVisibilityChanger getTileEntity() {
+        return tileEntityVisibilityChanger;
+    }
+
+    public static void changeHandlers(EntityVisibilityChanger entityVisibilityChanger1, PlayerVisibilityChanger playerVisibilityChanger1, TileEntityVisibilityChanger tileEntityVisibilityChanger1) {
+        entityVisibilityChanger = entityVisibilityChanger1;
+        playerVisibilityChanger = playerVisibilityChanger1;
+        tileEntityVisibilityChanger = tileEntityVisibilityChanger1;
     }
 }
