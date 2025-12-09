b1fa7e7c9638dd04fa784f3ef3cd172850cd8076
Cubicake <74905@stpeters.sa.edu.au>
2025-12-09 14:17:05 +1030

    Update interface for new AbstractBlockLocation

--- src/main/java/games/cubi/raycastedAntiESP/snapshot/BlockSnapshotManager.java
+++ src/main/java/games/cubi/raycastedAntiESP/snapshot/BlockSnapshotManager.java
@@ -1,8 +1,8 @@
 package games.cubi.raycastedAntiESP.snapshot;
 
-import games.cubi.raycastedAntiESP.locatables.Locatable;
+import games.cubi.raycastedAntiESP.locatables.block.AbstractBlockLocation;
 import org.bukkit.Material;
 
 public interface BlockSnapshotManager {
-    Material getMaterialAt(Locatable location);
+    Material getMaterialAt(AbstractBlockLocation location);
 }
