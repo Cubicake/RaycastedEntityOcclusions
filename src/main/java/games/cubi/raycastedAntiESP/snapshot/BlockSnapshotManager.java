3c053dbacc1889c150b43d97cff6a6527637119a
Cubicake <74905@stpeters.sa.edu.au>
2025-12-16 14:17:32 +1030

    change interface to supply boolean instead of Material

--- src/main/java/games/cubi/raycastedAntiESP/snapshot/BlockSnapshotManager.java
+++ src/main/java/games/cubi/raycastedAntiESP/snapshot/BlockSnapshotManager.java
@@ -1,8 +1,7 @@
 package games.cubi.raycastedAntiESP.snapshot;
 
 import games.cubi.raycastedAntiESP.locatables.block.AbstractBlockLocation;
-import org.bukkit.Material;
 
 public interface BlockSnapshotManager {
-    Material getMaterialAt(AbstractBlockLocation location);
+    boolean isBlockOccluding(AbstractBlockLocation location);
 }
