6a3a8b57689c4f4ed48d2e61aadc9268124b80cf
Cubicake <74905@stpeters.sa.edu.au>
2025-12-16 14:16:17 +1030

    add javadocs comment to communicate how the method works

--- src/main/java/games/cubi/raycastedAntiESP/snapshot/TileEntitySnapshotManager.java
+++ src/main/java/games/cubi/raycastedAntiESP/snapshot/TileEntitySnapshotManager.java
@@ -8,6 +8,13 @@ import java.util.UUID;
 
 public interface TileEntitySnapshotManager {
     Set<UUID> getPlayersWhoCanSee(Locatable tileEntity);
+
+    /**
+     * Returns the number of ticks since the player last saw the tile entity.
+     * @param player
+     * @param tileEntity
+     * @return -1 if the player cannot see the tile entity, otherwise the number of ticks since last seen.
+     */
     int getTicksSincePlayerSawTileEntity(UUID player, Locatable tileEntity);
     Set<BlockLocation> getTileEntitiesInChunk(UUID world, int x, int z);
 }
