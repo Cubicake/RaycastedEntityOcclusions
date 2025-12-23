59059248df48eef9988bc9e35deeaf5a83a79e11
Cubicake <74905@stpeters.sa.edu.au>
2025-12-23 22:26:48 +1030

    Implement TileEntityVisibilityChanger for Bukkit

---
+++ src/main/java/games/cubi/raycastedAntiESP/visibilitychangehandlers/tileentity/BukkitTVC.java
@@ -0,0 +1,68 @@
+package games.cubi.raycastedAntiESP.visibilitychangehandlers.tileentity;
+
+import games.cubi.raycastedAntiESP.locatables.block.AbstractBlockLocation;
+import games.cubi.raycastedAntiESP.locatables.block.BlockLocation;
+import games.cubi.raycastedAntiESP.snapshot.SnapshotManager;
+import games.cubi.raycastedAntiESP.visibilitychangehandlers.VisibilityChangeHandlers;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.TileState;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+public class BukkitTVC extends TileEntityCache implements TileEntityVisibilityChanger {
+    @Override
+    public void showTileEntityToPlayer(UUID player, BlockLocation tileEntity) {
+        setTileEntityVisibilityForPlayer(player, tileEntity, true);
+    }
+
+    @Override
+    public void hideTileEntityFromPlayer(UUID player, BlockLocation tileEntity) {
+        setTileEntityVisibilityForPlayer(player, tileEntity, false);
+    }
+
+    @Override
+    public void setTileEntityVisibilityForPlayer(UUID player, BlockLocation tileEntity, boolean visible) {
+        if (SnapshotManager.getTileEntitySnapshotManager().isTileEntityVisibleToPlayer(tileEntity, player) == visible) {
+            return;
+        }
+        addToTileEntityCache(player, tileEntity, visible);
+    }
+
+    @Override
+    public VisibilityChangeHandlers.TileEntityVisibilityChangerType getType() {
+        return VisibilityChangeHandlers.TileEntityVisibilityChangerType.BUKKIT;
+    }
+
+    @Override
+    public void processCache() {
+        processACache(flushTileEntityShowCache(), true);
+        processACache(flushTileEntityHideCache(), false);
+    }
+
+    private void processACache(Map<UUID, Set<AbstractBlockLocation>> cache, boolean show) {
+        for (Map.Entry<UUID, Set<AbstractBlockLocation>> entry : cache.entrySet()) {
+            UUID playerUUID = entry.getKey();
+            Set<AbstractBlockLocation> tileEntities = entry.getValue();
+            for (AbstractBlockLocation tileEntity : tileEntities) {
+                BlockState blockState = Bukkit.getWorld(tileEntity.world()).getBlockState(tileEntity.blockX(), tileEntity.blockY(), tileEntity.blockZ());
+
+                if (blockState instanceof TileState tileState) {
+                    Player player = Bukkit.getPlayer(playerUUID);
+                    if (player == null) return;
+                    Location location = blockState.getLocation();
+                    player.sendBlockChange(location, blockState.getBlockData());
+                    player.sendBlockUpdate(location, tileState);
+                }
+                else {
+                    //Logger.warning("Tried to show tile entity at " + location + " to "+p.getName()+" but it was not a TileState! Block type: " + block.getType()+". Removing from the list of tile entities.");
+                    SnapshotManager.getTileEntitySnapshotManager().removeFromTileEntityLastSeenMap(tileEntity);
+                }
+            }
+        }
+    }
+}
