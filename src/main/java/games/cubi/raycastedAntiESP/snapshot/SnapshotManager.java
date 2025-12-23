2d7dc391ae5a5e1ccc4680881f80ba05479f2fab
Cubicake <74905@stpeters.sa.edu.au>
2025-12-23 22:25:46 +1030

    Add enums to check which snapshot managers are present

--- src/main/java/games/cubi/raycastedAntiESP/snapshot/SnapshotManager.java
+++ src/main/java/games/cubi/raycastedAntiESP/snapshot/SnapshotManager.java
@@ -1,9 +1,8 @@
 package games.cubi.raycastedAntiESP.snapshot;
 
-import games.cubi.raycastedAntiESP.visibilitychangehandlers.EntityVisibilityChanger;
-import games.cubi.raycastedAntiESP.visibilitychangehandlers.PlayerVisibilityChanger;
-import games.cubi.raycastedAntiESP.visibilitychangehandlers.TileEntityVisibilityChanger;
-import org.jspecify.annotations.Nullable;
+import games.cubi.raycastedAntiESP.snapshot.block.BlockSnapshotManager;
+import games.cubi.raycastedAntiESP.snapshot.entity.EntitySnapshotManager;
+import games.cubi.raycastedAntiESP.snapshot.tileentity.TileEntitySnapshotManager;
 
 public class SnapshotManager {
     private static BlockSnapshotManager blockSnapshotManager;
@@ -11,6 +10,22 @@ public class SnapshotManager {
     private static TileEntitySnapshotManager tileEntitySnapshotManager;
 
     private SnapshotManager() {}
+
+    public enum EntitySnapshotManagerType {
+        BUKKIT,
+    }
+
+    public enum BlockSnapshotManagerType {
+        BUKKIT,
+    }
+
+    public enum TileEntitySnapshotManagerType {
+        BUKKIT,
+    }
+
+    private static EntitySnapshotManagerType entitySnapshotManagerType;
+    private static BlockSnapshotManagerType blockSnapshotManagerType;
+    private static TileEntitySnapshotManagerType tileEntitySnapshotManagerType;
 
     public static void initialise(BlockSnapshotManager blockSnapshotManager1, EntitySnapshotManager entitySnapshotManager1, TileEntitySnapshotManager tileEntitySnapshotManager1) {
         changeManagers(blockSnapshotManager1, entitySnapshotManager1, tileEntitySnapshotManager1);
@@ -28,9 +43,25 @@ public class SnapshotManager {
         return tileEntitySnapshotManager;
     }
 
+    public static EntitySnapshotManagerType entitySnapshotManagerType() {
+        return entitySnapshotManagerType;
+    }
+
+    public static BlockSnapshotManagerType blockSnapshotManagerType() {
+        return blockSnapshotManagerType;
+    }
+
+    public static TileEntitySnapshotManagerType tileEntitySnapshotManagerType() {
+        return tileEntitySnapshotManagerType;
+    }
+
     public static void changeManagers(BlockSnapshotManager blockSnapshotManager1, EntitySnapshotManager entitySnapshotManager1, TileEntitySnapshotManager tileEntitySnapshotManager1) {
         blockSnapshotManager = blockSnapshotManager1;
         entitySnapshotManager = entitySnapshotManager1;
         tileEntitySnapshotManager = tileEntitySnapshotManager1;
+
+        entitySnapshotManagerType = entitySnapshotManager1.getType();
+        blockSnapshotManagerType = blockSnapshotManager1.getType();
+        tileEntitySnapshotManagerType = tileEntitySnapshotManager1.getType();
     }
 }
