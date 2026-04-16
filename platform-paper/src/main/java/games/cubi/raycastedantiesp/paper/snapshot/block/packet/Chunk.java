36ddc2ab4b87e97c3ad5e1833c20dcabdfdd97b6
Cubicake <74905@stpeters.sa.edu.au>
2026-04-16 20:05:55 +0930

    Superseded by the new tile/entity view interfaces and their packet implementations

--- platform-paper/src/main/java/games/cubi/raycastedantiesp/paper/snapshot/block/packet/Chunk.java
+++
@@ -1,5 +0,0 @@
-package games.cubi.raycastedantiesp.paper.snapshot.block.packet;
-
-import java.util.UUID;
-@Deprecated(forRemoval = true)
-public record Chunk(UUID world, int chunkX, int chunkZ) {}
