fcaffbabcc257eac6e929d6318a4a30971988d36
Cubicake <74905@stpeters.sa.edu.au>
2026-04-17 21:26:14 +0930

    Implementation of EntityLocatable with thread safety as needed for netty-based systems

---
+++ locatable-lib/src/main/java/games/cubi/locatables/implementations/NettyEntityLocatable.java
@@ -0,0 +1,326 @@
+package games.cubi.locatables.implementations;
+
+import games.cubi.locatables.EntityLocatable;
+import games.cubi.locatables.MutableLocatable;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Designed for use with netty-based systems, where entity data updates only ever come from one thread, but reads may come from multiple threads. This is however not enforced, and must be kept in mind when using this class.
+ * A representation of an entity for a specific player.
+ */
+public class NettyEntityLocatable implements EntityLocatable {
+    // immutable fields
+    private final int entityID;
+    private final UUID entityUUID;
+    private final SpawnType spawnType;
+    private final Object entityType; // PacketEvents stores EntityType and PaintingType here
+    private final Object paintingDirection; //Null if type is not painting
+
+    // Netty mutatable fields. Should NEVER be mutated from the engine thread, but reads are fine.
+    private volatile UUID world;
+    private volatile double x, y, z;
+    private volatile float yaw;
+    private volatile float pitch;
+    private volatile float headYaw;
+    private volatile double velocityX;
+    private volatile double velocityY;
+    private volatile double velocityZ;
+    private volatile boolean onGround = true;
+
+    private volatile int entityData;
+    private volatile List<?> metadata = List.of();
+    private volatile List<?> equipment = List.of();
+    private volatile int[] passengerIDs = new int[0];
+    private volatile Object packetReplayData; // For use storing the packets we can't be bothered to directly store, with all cached packets being sent back out to the client when entity is visible again.
+
+    // mutatable by several threads (engine and netty), may need to investigate atomic updates for thread safety
+    private volatile int lastChecked;
+    private volatile boolean clientVisible = true;
+
+    // engine thread mutable, reads from netty and engine.
+    private volatile boolean visible = true;
+
+    public NettyEntityLocatable(UUID world, double x, double y, double z, int entityID, UUID entityUUID, SpawnType spawnType, Object entityType) {
+        this.world = world;
+        this.x = x; this.y = y; this.z = z;
+
+        this.entityID = entityID;
+        this.entityUUID = entityUUID;
+        this.spawnType = spawnType;
+        this.entityType = entityType;
+        this.paintingDirection = null;
+    }
+
+    public NettyEntityLocatable(UUID world, double x, double y, double z, int entityID, UUID entityUUID, SpawnType spawnType, Object paintingType, Object paintingDirection) {
+        this.world = world;
+        this.x = x; this.y = y; this.z = z;
+
+        this.entityID = entityID;
+        this.entityUUID = entityUUID;
+        this.spawnType = spawnType;
+        this.entityType = paintingType;
+        this.paintingDirection = paintingDirection;
+    }
+
+    @Override
+    public LocatableType getType() {
+        return LocatableType.ThreadSafeEntity;
+    }
+
+    @Override
+    public int entityID() {
+        return entityID;
+    }
+
+    @Override
+    public UUID entityUUID() {
+        return entityUUID;
+    }
+
+    @Override
+    public boolean visible() {
+        return visible;
+    }
+
+    /**
+     * Wherever possible, effort should be made to only call this from the engine thread, for thread safety reasons.
+     */
+    @Override
+    public EntityLocatable setVisible(boolean visible) {
+        this.visible = visible;
+        return this;
+    }
+
+    @Override
+    public int lastChecked() {
+        return lastChecked;
+    }
+// todo it may be that this is only ever set by the engine thread? If so, just volatile annotation may be safe enough, as no two engine threads should update a single player at the same time (add guard lock for this)
+    @Override
+    public EntityLocatable setLastChecked(int lastChecked) {
+        this.lastChecked = lastChecked;
+        return this;
+    }
+
+    @Override
+    public boolean clientVisible() {
+        return clientVisible;
+    }
+
+    @Override
+    public EntityLocatable setClientVisible(boolean clientVisible) {
+        this.clientVisible = clientVisible;
+        return this;
+    }
+
+    @Override
+    public SpawnType spawnType() {
+        return spawnType;
+    }
+
+    @Override
+    public float yaw() {
+        return yaw;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setYaw(float yaw) {
+        this.yaw = yaw;
+        return this;
+    }
+
+    @Override
+    public float pitch() {
+        return pitch;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setPitch(float pitch) {
+        this.pitch = pitch;
+        return this;
+    }
+
+    @Override
+    public float headYaw() {
+        return headYaw;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setHeadYaw(float headYaw) {
+        this.headYaw = headYaw;
+        return this;
+    }
+
+    @Override
+    public double velocityX() {
+        return velocityX;
+    }
+
+    @Override
+    public double velocityY() {
+        return velocityY;
+    }
+
+    @Override
+    public double velocityZ() {
+        return velocityZ;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setVelocity(double velocityX, double velocityY, double velocityZ) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.velocityZ = velocityZ;
+        return this;
+    }
+
+    @Override
+    public boolean onGround() {
+        return onGround;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setOnGround(boolean onGround) {
+        this.onGround = onGround;
+        return this;
+    }
+
+    @Override
+    public Object entityType() {
+        return entityType;
+    }
+
+    @Override
+    public int entityData() {
+        return entityData;
+    }
+
+    @Override
+    public EntityLocatable setEntityData(int entityData) {
+        this.entityData = entityData;
+        return this;
+    }
+
+    @Override
+    public Object paintingType() {
+        return entityType;
+    }
+
+    @Override
+    public Object paintingDirection() {
+        return paintingDirection;
+    }
+
+    @Override
+    public List<?> metadata() {
+        return metadata;
+    }
+
+    /**
+     * THIS METHOD IS ONLY SAFE TO CALL FROM THE PLAYER's NETTY THREAD
+     */
+    @Override
+    public EntityLocatable setMetadata(List<?> metadata) {
+        this.metadata = metadata == null ? List.of() : List.copyOf(metadata);
+        return this;
+    }
+
+    @Override
+    public List<?> equipment() {
+        return equipment;
+    }
+
+    @Override
+    public EntityLocatable setEquipment(List<?> equipment) {
+        this.equipment = equipment == null ? List.of() : List.copyOf(equipment);
+        return this;
+    }
+
+    @Override
+    public int[] passengerIDs() {
+        return passengerIDs.clone();
+    }
+
+    @Override
+    public EntityLocatable setPassengerIDs(int[] passengerIDs) {
+        this.passengerIDs = passengerIDs == null ? new int[0] : passengerIDs.clone();
+        return this;
+    }
+
+    @Override
+    public Object packetReplayData() {
+        return packetReplayData;
+    }
+
+    @Override
+    public EntityLocatable setPacketReplayData(Object packetReplayData) {
+        this.packetReplayData = packetReplayData;
+        return this;
+    }
+
+    @Override
+    public double x() {
+        return x;
+    }
+
+    @Override
+    public double y() {
+        return y;
+    }
+
+    @Override
+    public double z() {
+        return z;
+    }
+
+    @Override
+    public UUID world() {
+        return world;
+    }
+
+    @Override
+    public MutableLocatable clonePlainAndCentreIfBlockLocation() {
+        return new MutableLocatableImpl(world, x, y, z);
+    }
+
+    @Override
+    public MutableLocatable setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    @Override
+    public MutableLocatable setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+    @Override
+    public MutableLocatable setZ(double z) {
+        this.z = z;
+        return this;
+    }
+
+    @Override
+    public MutableLocatable setWorld(UUID world) {
+        this.world = world;
+        return this;
+    }
+}
