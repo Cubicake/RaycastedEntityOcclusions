c34bfdb61f05ce9460200abc27478b161c01ad08
Cubicake <74905@stpeters.sa.edu.au>
2026-04-17 21:29:12 +0930

    View interfaces

---
+++ core/src/main/java/games/cubi/raycastedantiesp/core/view/TileEntityView.java
@@ -0,0 +1,35 @@
+package games.cubi.raycastedantiesp.core.view;
+
+import games.cubi.locatables.BlockLocatable;
+import games.cubi.locatables.implementations.ImmutableBlockLocatable;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface TileEntityView {
+    void upsertTileEntity(BlockLocatable location, int currentTick);
+
+    void insertIfAbsent(BlockLocatable location);
+
+    void removeTileEntity(BlockLocatable location, int currentTick);
+
+    TrackedTileEntity<?> getTrackedTileEntity(BlockLocatable location);
+
+    TrackedTileEntity<?> getTrackedTileEntity(ImmutableBlockLocatable location);
+
+    boolean isVisible(BlockLocatable location, int currentTick);
+
+    void setVisibility(BlockLocatable location, boolean visible, int currentTick);
+
+    Collection<BlockLocatable> getKnownTileEntities();
+
+    Collection<BlockLocatable> getNeedingRecheck(int recheckTicks, int currentTick);
+
+    boolean hasPendingTransitions();
+
+    List<TileEntityViewTransition> drainTransitions();
+
+    interface Factory {
+        TileEntityView createTileEntityView();
+    }
+}
