4c21c16e3b30739d436d6afde5c5cd4e73f94d4e
Cubicake <74905@stpeters.sa.edu.au>
2026-04-17 21:35:02 +0930

    Cache changes until on player's netty thread

---
+++ core/src/main/java/games/cubi/raycastedantiesp/core/view/EntityViewTransition.java
@@ -0,0 +1,12 @@
+package games.cubi.raycastedantiesp.core.view;
+
+import java.util.UUID;
+
+// Used to cache visibility changes until the player's netty thread next processes
+public record EntityViewTransition(Type type, UUID targetUUID, int entityID) {
+    public enum Type {
+        SHOW,
+        HIDE,
+        FORGET,
+    }
+}
