43e11210cfc09560e3affd5ed40671a9568c2a86
Cubicake <74905@stpeters.sa.edu.au>
2026-04-17 21:28:54 +0930

    getType does not exist anymore

--- packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/PacketEventsBlockSnapshotManager.java
+++ packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/PacketEventsBlockSnapshotManager.java
@@ -1,11 +1,6 @@
 package games.cubi.raycastedantiesp.packetevents;
 
 import games.cubi.raycastedantiesp.core.packets.core.PacketBlockSnapshotManager;
-import games.cubi.raycastedantiesp.core.snapshot.SnapshotManager;
 
 public class PacketEventsBlockSnapshotManager extends PacketBlockSnapshotManager {
-    @Override
-    public SnapshotManager.SnapshotManagerType getType() {
-        return SnapshotManager.SnapshotManagerType.PACKETEVENTS;
-    }
 }
