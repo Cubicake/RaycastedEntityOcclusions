c6e4af9cdb698969c172ac2d8d2838ac515713d6
Cubicake <74905@stpeters.sa.edu.au>
2026-04-17 21:25:16 +0930

    A locatable that represents a per-player entity

---
+++ locatable-lib/src/main/java/games/cubi/locatables/EntityLocatable.java
@@ -0,0 +1,73 @@
+package games.cubi.locatables;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Per-player platform-independent representation of an entity
+ */
+public interface EntityLocatable extends MutableLocatable {
+    enum SpawnType {
+        LIVING,
+        ENTITY,
+        PAINTING,
+        PLAYER,
+    }
+
+    int entityID();
+
+    UUID entityUUID();
+
+    boolean visible();
+    EntityLocatable setVisible(boolean visible);
+
+    int lastChecked();
+    EntityLocatable setLastChecked(int lastChecked);
+
+    boolean clientVisible();
+    EntityLocatable setClientVisible(boolean clientVisible);
+
+    SpawnType spawnType();
+
+    float yaw();
+    EntityLocatable setYaw(float yaw);
+
+    float pitch();
+    EntityLocatable setPitch(float pitch);
+
+    float headYaw();
+    EntityLocatable setHeadYaw(float headYaw);
+
+    double velocityX();
+    double velocityY();
+    double velocityZ();
+    EntityLocatable setVelocity(double velocityX, double velocityY, double velocityZ);
+
+    boolean onGround();
+    EntityLocatable setOnGround(boolean onGround);
+
+    Object entityType();
+
+    int entityData();
+    EntityLocatable setEntityData(int entityData);
+
+    Object paintingType();
+
+    Object paintingDirection();
+
+    List<?> metadata();
+    EntityLocatable setMetadata(List<?> metadata);
+
+    List<?> equipment();
+    EntityLocatable setEquipment(List<?> equipment);
+
+    int[] passengerIDs();
+    EntityLocatable setPassengerIDs(int[] passengerIDs);
+
+    Object packetReplayData();
+    EntityLocatable setPacketReplayData(Object packetReplayData);
+
+    default <T> T cast() {
+        return (T) this;
+    }
+}
