98b910fd2ecac2efed1bff7dd4c336d6025685e8
Cubicake <74905@stpeters.sa.edu.au>
2026-04-19 15:59:59 +0930

    Update registry for new merged factory

--- core/src/main/java/games/cubi/raycastedantiesp/core/view/ViewRegistry.java
+++ core/src/main/java/games/cubi/raycastedantiesp/core/view/ViewRegistry.java
@@ -1,30 +1,26 @@
 package games.cubi.raycastedantiesp.core.view;
 
 import games.cubi.logs.Logger;
-import games.cubi.raycastedantiesp.core.snapshot.PlayerBlockSnapshotManager;
 
 public final class ViewRegistry {
     private static EntityView.Factory entityViewFactory;
-    private static TileEntityView.Factory tileEntityViewFactory;
-    private static PlayerBlockSnapshotManager.Factory blockSnapshotFactory;
+    private static BlockView.Factory blockViewFactory;
 
     private ViewRegistry() {}
 
     public static void initialise(
-            PlayerBlockSnapshotManager.Factory blockSnapshotFactory1,
-            EntityView.Factory entityViewFactory1,
-            TileEntityView.Factory tileEntityViewFactory1
+            BlockView.Factory blockViewFactory1,
+            EntityView.Factory entityViewFactory1
     ) {
-        blockSnapshotFactory = blockSnapshotFactory1;
+        blockViewFactory = blockViewFactory1;
         entityViewFactory = entityViewFactory1;
-        tileEntityViewFactory = tileEntityViewFactory1;
     }
 
-    public static PlayerBlockSnapshotManager createBlockSnapshotManager() {
-        if (blockSnapshotFactory == null) {
-            Logger.error(new IllegalStateException("Block snapshot factory is null. Did you forget to initialise ViewRegistry?"), 1, ViewRegistry.class);
+    public static BlockView createBlockView() {
+        if (blockViewFactory == null) {
+            Logger.error(new IllegalStateException("Block view factory is null. Did you forget to initialise ViewRegistry?"), 1, ViewRegistry.class);
         }
-        return blockSnapshotFactory.createPlayerBlockSnapshotManager();
+        return blockViewFactory.createBlockView();
     }
 
     public static EntityView<?> createEntityView() {
@@ -32,12 +28,5 @@ public final class ViewRegistry {
             Logger.error(new IllegalStateException("Entity view factory is null. Did you forget to initialise ViewRegistry?"), 1, ViewRegistry.class);
         }
         return entityViewFactory.createEntityView();
-    }
-
-    public static TileEntityView createTileEntityView() {
-        if (tileEntityViewFactory == null) {
-            Logger.error(new IllegalStateException("Tile entity view factory is null. Did you forget to initialise ViewRegistry?"), 1, ViewRegistry.class);
-        }
-        return tileEntityViewFactory.createTileEntityView();
     }
 }
