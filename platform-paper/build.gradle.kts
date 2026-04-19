7e1f5d5bd38e181816cc386ebe8991f498b12de8
Aquiles <69038261+zlAquiles@users.noreply.github.com>
2026-04-19 17:57:16 -0300

    Added the optional WorldGuard dependency.

--- platform-paper/build.gradle.kts
+++ platform-paper/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.enginehub.org/repo/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
 }
@@ -17,6 +18,7 @@ repositories {
 dependencies {
     paperweight.paperDevBundle("1.20.6-R0.1-SNAPSHOT")
     compileOnly("com.github.retrooper:packetevents-spigot:2.8.0")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
     compileOnly("org.spongepowered:configurate-core:4.2.0")
     compileOnly("org.spongepowered:configurate-yaml:4.2.0")
 
