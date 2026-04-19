b5e78cc0ff776c1ed438e823739fe031401139c6
Cubicake <74905@stpeters.sa.edu.au>
2026-04-19 19:59:34 +0930

    Functionally identical

--- locatable-lib/src/main/java/games/cubi/locatables/Locatable.java
+++ locatable-lib/src/main/java/games/cubi/locatables/Locatable.java
@@ -93,9 +93,9 @@ public sealed interface Locatable extends ChunkSectionLocatable permits MutableL
         int hash = 3;
 
         hash = 19 * hash + this.world().hashCode();
-        hash = 19 * hash + (int) (Double.doubleToLongBits(this.x()) ^ (Double.doubleToLongBits(this.x()) >>> 32));
-        hash = 19 * hash + (int) (Double.doubleToLongBits(this.y()) ^ (Double.doubleToLongBits(this.y()) >>> 32));
-        hash = 19 * hash + (int) (Double.doubleToLongBits(this.z()) ^ (Double.doubleToLongBits(this.z()) >>> 32));
+        hash = 19 * hash + Long.hashCode(Double.doubleToLongBits(x()));
+        hash = 19 * hash + Long.hashCode(Double.doubleToLongBits(y()));
+        hash = 19 * hash + Long.hashCode(Double.doubleToLongBits(z()));
         return hash;
     }
 
