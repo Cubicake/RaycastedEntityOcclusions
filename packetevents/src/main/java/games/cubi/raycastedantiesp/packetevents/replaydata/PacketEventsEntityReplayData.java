f60934a0233630cb3c87c13fa7e0d6505441b1f6
Cubicake <74905@stpeters.sa.edu.au>
2026-05-03 16:10:45 +0930

    Make ReplayData an append-only queue.

--- packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/replaydata/PacketEventsEntityReplayData.java
+++ packetevents/src/main/java/games/cubi/raycastedantiesp/packetevents/replaydata/PacketEventsEntityReplayData.java
@@ -1,10 +1,7 @@
 package games.cubi.raycastedantiesp.packetevents.replaydata;
 
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityEffect;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityEquipment;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityVelocity;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetPassengers;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerInfoUpdate;
 
 import games.cubi.raycastedantiesp.core.utils.Clearable;
 
@@ -13,95 +10,37 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public sealed interface PacketEventsEntityReplayData extends Clearable permits PacketEventsEntityReplayData.Impl, PacketEventsPlayerReplayData {
+public sealed interface PacketEventsEntityReplayData extends Clearable permits PacketEventsEntityReplayData.Impl {
 
-    void addEffectPacket(WrapperPlayServerEntityEffect packet);
-
-    List<WrapperPlayServerEntityEffect> effectPackets();
-
-    WrapperPlayServerEntityMetadata metadataPacket();
-
-    void setMetadataPacket(WrapperPlayServerEntityMetadata metadataPacket);
-
-    WrapperPlayServerEntityEquipment equipmentPacket();
-
-    void setEquipmentPacket(WrapperPlayServerEntityEquipment equipmentPacket);
-
-    WrapperPlayServerEntityVelocity velocityPacket();
-
-    void setVelocityPacket(WrapperPlayServerEntityVelocity velocityPacket);
-
-    WrapperPlayServerSetPassengers passengersPacket();
-
-    void setPassengersPacket(WrapperPlayServerSetPassengers passengersPacket);
+    void addPacket(PacketWrapper<?> packet);
 
     /**
-     * @return The same object, as a {@link PacketEventsPlayerReplayData}, or null if this is not a player entity
+     * @return The actual queue, do not modify.
      */
-    PacketEventsPlayerReplayData asPlayerReplayData();
+    Queue<PacketWrapper<?>> getPackets();
 
     static PacketEventsEntityReplayData create() {
         return new Impl();
     }
 
-    sealed class Impl implements PacketEventsEntityReplayData permits PacketEventsPlayerReplayData.Impl {
-        private final Queue<WrapperPlayServerEntityEffect> effectPackets = new ArrayDeque<>();
-        private volatile WrapperPlayServerEntityMetadata metadataPacket;
-        private volatile WrapperPlayServerEntityEquipment equipmentPacket;
-        private volatile WrapperPlayServerEntityVelocity velocityPacket;
-        private volatile WrapperPlayServerSetPassengers passengersPacket;
+    final class Impl implements PacketEventsEntityReplayData {
+        private final Queue<PacketWrapper<?>> packets = new ArrayDeque<>();
 
-        public void addEffectPacket(WrapperPlayServerEntityEffect packet) {
-            effectPackets.add(packet);
+        public Impl() {
         }
 
-        public List<WrapperPlayServerEntityEffect> effectPackets() {
-            return List.copyOf(effectPackets);
+        public void addPacket(PacketWrapper<?> packet) {
+            packets.add(packet);
         }
 
-        public WrapperPlayServerEntityMetadata metadataPacket() {
-            return metadataPacket;
-        }
-
-        public void setMetadataPacket(WrapperPlayServerEntityMetadata metadataPacket) {
-            this.metadataPacket = metadataPacket;
-        }
-
-        public WrapperPlayServerEntityEquipment equipmentPacket() {
-            return equipmentPacket;
-        }
-
-        public void setEquipmentPacket(WrapperPlayServerEntityEquipment equipmentPacket) {
-            this.equipmentPacket = equipmentPacket;
-        }
-
-        public WrapperPlayServerEntityVelocity velocityPacket() {
-            return velocityPacket;
-        }
-
-        public void setVelocityPacket(WrapperPlayServerEntityVelocity velocityPacket) {
-            this.velocityPacket = velocityPacket;
-        }
-
-        public WrapperPlayServerSetPassengers passengersPacket() {
-            return passengersPacket;
-        }
-
-        public void setPassengersPacket(WrapperPlayServerSetPassengers passengersPacket) {
-            this.passengersPacket = passengersPacket;
-        }
-
-        public PacketEventsPlayerReplayData asPlayerReplayData() {
-            return null;
+        @Override
+        public Queue<PacketWrapper<?>> getPackets() {
+            return packets;
         }
 
         @Override
         public void clear() {
-            effectPackets.clear();
-            metadataPacket = null;
-            equipmentPacket = null;
-            velocityPacket = null;
-            passengersPacket = null;
+            packets.clear();
         }
     }
 
