a6c79d4bbe92525097ee55f66f5aae6f85169483
Cubicake <74905@stpeters.sa.edu.au>
2026-05-04 22:47:23 +0930

    Start documenting packets and what PE class they map to

---
+++ core/src/main/java/games/cubi/raycastedantiesp/core/utils/Packet.java
@@ -0,0 +1,37 @@
+package games.cubi.raycastedantiesp.core.utils;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+public @interface Packet {
+    //todo note: spawn living entity and painting are really old and no longer used, delete them from code maybe
+    enum Packets {
+        @Until("1.9")
+        SPAWN_LIVING_ENTITY("N/A", "WrapperPlayServerSpawnEntityLiving"),
+        SPAWN_ENTITY("https://minecraft.wiki/w/Java_Edition_protocol/Packets#Spawn_Entity", "WrapperPlayServerSpawnEntity"),
+        ENTITY_ANIMATION("https://minecraft.wiki/w/Java_Edition_protocol/Packets#Entity_Animation", "WrapperPlayServerEntityAnimation"),
+        ENTITY_EVENT("https://minecraft.wiki/w/Java_Edition_protocol/Packets#Entity_Event", "WrapperPlayServerEntityStatus"),
+        HURT_ANIMATION("https://minecraft.wiki/w/Java_Edition_protocol/Packets#Hurt_Animation", "WrapperPlayServerHurtAnimation"),
+        ;
+
+        Packets(String wikiURL, String PEName) {
+        }
+    }
+
+    Packets value();
+
+    @Documented
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Since {
+        String value();
+    }
+
+    @Documented
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Until {
+        String value();
+    }
+}
